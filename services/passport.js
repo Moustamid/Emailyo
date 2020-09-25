d6510c9ab1d1275d15f94290aca2a36110a8624a
MOUSTAMID <karim1992moustamid@gmail.com>
2020-09-25 16:54:34 +0100

    mongo-ExpressRouting-Oauth with google validated

--- services/passport.js
+++ services/passport.js
@@ -23,6 +23,7 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
+       
       User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           // we already have a record with the given profile ID
@@ -36,6 +37,10 @@ passport.use(
             .then(user => done(null, user));
         }
       });
+      
     }
   )
 );
+
+
+
