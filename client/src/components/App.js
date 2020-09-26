2543ea40befef08a254c598f31ab69749fdea360
MOUSTAMID <karim1992moustamid@gmail.com>
2020-09-27 00:06:19 +0100

    adding axios and redux thunk

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,7 @@ const surveyNew = () => <h2>surveyNew</h2>
 
 const App = () => {
    return (
-      <div>
+      <div className="container">
         <BrowserRouter>
            <div>
              <Header/>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,14 @@ class Header extends Component {
    render() {
      return (
         <div>
-           <h2> Header</h2>
+          <nav>
+            <div class="nav-wrapper">
+              <a href="#" class="left brand-logo">Emailyo</a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Login With Google</a></li>
+              </ul>
+            </div>
+          </nav>
         </div>
      );
    }
